14d7a279bbfb4d66e7820533f0aad9e4b8b848be
eliastg <eliastrenard@gmail.com>
2021-01-25 02:14:25 -0500

    Fixed some compilation and security issues.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Configuration from './config/Configuration';
 function App() {
   return (
     <div className="App">
-      <div><h2>Products of <a target="_blank" href={Configuration.STORE_URL}>etgtest Store</a></h2></div>
+      <div><h2>Products of <a rel="noreferrer" target="_blank" href={Configuration.STORE_URL}>etgtest Store</a></h2></div>
       <ProductList/>
     </div>
   );
--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Tr, Td } from 'react-responsive-list';
 import Configuration from '../../config/Configuration';
 import './ProductListItem.css';
 
-class ProductList extends Component
-{
-    render(){
+class ProductList extends Component {
+    render() {
         return (
             <Tr>
                 <Td>
-                    <div><img src={this.props.product.image.src}/></div>
+                    <div><img alt="Product" src={this.props.product.image.src} /></div>
                 </Td>
                 <Td>
-                    <a target="_blank" href={`${Configuration.STORE_URL}/products/${this.props.product.handle}`}>
+                    <a rel="noreferrer" target="_blank" href={`${Configuration.STORE_URL}/products/${this.props.product.handle}`}>
                         <h3>{this.props.product.title}</h3>
                     </a>
                     <h1>{this.props.product.variants[0].price + ' EUR'}</h1>
